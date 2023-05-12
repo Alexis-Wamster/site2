506315a1ce383c0e35b12f9bdf1bb6910ac248c6
Alex Wamster <wamsteralexis@gmail.com>
2023-05-12 20:05:47 +0200

    ajout des premier fichier

---
+++ html/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>test</title>
+</head>
+<body>
+	<?php include("php/votre_fichier_php.php"); ?>
+	<p>Le nom est : <?php echo $nom; ?></p>
+</body>
+</html>
